4956d8f3be17e6bc45999a5d6eded497eaabbf28
Howard Liu <zhihliu@stonybrook.edu>
2022-11-29 22:43:51 -0500

    embeded youtube player example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import YouTube from 'react-youtube';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App(){
+  
+  let playlist = [
+    "NUiwTwpPGRk",
+    "EG_CmgmltWQ",
+    "FoYK1AinPnE"
+  ];
 
-export default App;
+  let index = 0;
+  const [ytbID, setYtbID] = useState(playlist[index]);
+
+
+    let player;
+
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+
+    function _onReady(event) {
+      // access to player in all event handlers via event.target
+      player = event.target;
+    }
+
+    function handlePause() {
+      player.pauseVideo();
+    }
+
+    function handleStart() {
+      player.playVideo();
+    }
+
+    function handleNext() {
+      index++;
+      setYtbID(playlist[index]);
+      player.playVideo();
+    }
+
+    return <div>
+      <YouTube videoId={ytbID} opts={opts} onReady={_onReady} />
+      <button onClick={handlePause}>pause</button> <button onClick={handleStart}>Start</button>
+      <button onClick={handleNext}>NextSong</button>
+    </div>;
+  }
